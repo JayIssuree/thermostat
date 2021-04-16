2a992e86393ebc2a3cd61e115d7513ec5b1f2025
JayIssuree <jay_issuree@hotmail.co.uk>
2021-04-16 13:04:39 +0100

    completed fizzbuzz in javascript

--- fizzbuzz/jasmine/spec/FizzBuzzSpec.js
+++ fizzbuzz/jasmine/spec/FizzBuzzSpec.js
@@ -1,23 +1,48 @@
-describe('FizzBuzz', function() {
+describe("FizzBuzz", function () {
+  beforeEach(function () {
+    subject = new FizzBuzz();
+  });
 
-    beforeEach(function(){
-        subject = new FizzBuzz();
-    })
-
-    describe('knows when a number is', function(){
-
-        it('divisible by 3', function(){
-            expect(subject._isDivisibleByThree(3)).toBe(true);
-        });
-
+  describe("knows when a number is", function () {
+    it("divisible by 3", function () {
+      expect(subject._isDivisibleByThree(6)).toBe(true);
     });
 
-    describe('knows when a number is not', function(){
-
-        it('dibisible by 3', function(){
-            expect(subject._isDivisibleByThree(1)).toBe(false);
-        });
-
+    it("divisible by 5", function () {
+      expect(subject._isDivisibleByFive(10)).toBe(true);
     });
 
+    it("divisible by 15", function () {
+      expect(subject._isDivisibleByFifteen(30)).toBe(true);
+    });
+  });
+
+  describe("knows when a number is not", function () {
+    it("dibisible by 3", function () {
+      expect(subject._isDivisibleByThree(4)).toBe(false);
+    });
+
+    it("dibisible by 5", function () {
+      expect(subject._isDivisibleByFive(8)).toBe(false);
+    });
+
+    it("divisible by 15", function () {
+      expect(subject._isDivisibleByFifteen(9)).toBe(false);
+    });
+  });
+
+  describe("says", function () {
+    it("Fizz when divisible by 3", function () {
+      expect(subject.says(3)).toEqual("Fizz");
+    });
+    it("Buzz when divisible by 5", function () {
+      expect(subject.says(5)).toEqual("Buzz");
+    });
+    it("Fizzbuzz when divisible by 3 & 5", function () {
+      expect(subject.says(15)).toEqual("FizzBuzz");
+    });
+    it("returns the number when none are true", function () {
+      expect(subject.says(4)).toEqual(4);
+    });
+  });
 });
--- fizzbuzz/jasmine/src/FizzBuzz.js
+++ fizzbuzz/jasmine/src/FizzBuzz.js
@@ -1,7 +1,29 @@
 class FizzBuzz {
+  _isDivisibleBy(number, devisor) {
+    return number % devisor === 0;
+  }
 
-    _isDivisibleByThree(number){
-        return (number % 3 === 0)
-    };
-    
-};
+  _isDivisibleByThree(number) {
+    return this._isDivisibleBy(number, 3);
+  }
+
+  _isDivisibleByFive(number) {
+    return this._isDivisibleBy(number, 5);
+  }
+
+  _isDivisibleByFifteen(number) {
+    return this._isDivisibleBy(number, 15);
+  }
+
+  says(number) {
+    if (this._isDivisibleByThree(number) && this._isDivisibleByFive(number)) {
+      return "FizzBuzz";
+    } else if (this._isDivisibleByFive(number)) {
+      return "Buzz";
+    } else if (this._isDivisibleByThree(number)) {
+      return "Fizz";
+    } else {
+      return number;
+    }
+  }
+}
